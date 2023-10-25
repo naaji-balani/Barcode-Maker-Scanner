e3640fae86f438e9c6472f27e975437e23c0ab07
developer-techventions <112747410+developer-techventions@users.noreply.github.com>
2023-10-25 22:36:49 +0530

    Update

--- Barcode Scanner/Assets/Scripts/GoogleSheetReader.cs
+++ Barcode Scanner/Assets/Scripts/GoogleSheetReader.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.Networking;
+using SimpleJSON;
+using System.Collections.Generic;
 using System.Collections;
 
 public class GoogleSheetReader : MonoBehaviour
 {
     public string googleSheetAPIEndpoint = "YOUR_GOOGLE_SHEET_API_ENDPOINT";
     public string apiKey = "YOUR_API_KEY";
+    private List<string> mobileNumbers = new List<string>();
 
     void Start()
     {
@@ -15,7 +18,6 @@ public class GoogleSheetReader : MonoBehaviour
     IEnumerator ReadDataFromGoogleSheet()
     {
         string url = googleSheetAPIEndpoint + "?key=" + apiKey;
-        Debug.Log(url);
         UnityWebRequest www = UnityWebRequest.Get(url);
 
         yield return www.SendWebRequest();
@@ -27,10 +29,57 @@ public class GoogleSheetReader : MonoBehaviour
         else
         {
             string jsonResult = www.downloadHandler.text;
+            Debug.Log("JSON Data: " + jsonResult);
 
-            // Parse the JSON data and process it as needed.
-            // You'll need to extract the data you want from the JSON response.
-            Debug.Log(jsonResult);
+            // Parse JSON manually
+            JSONNode jsonData = JSON.Parse(jsonResult);
+
+            // Access the "values" array
+            JSONArray valuesArray = jsonData["values"].AsArray;
+
+            if (valuesArray.Count > 0)
+            {
+                // Find the index of the "Number" column (assuming it's in the first row)
+                int numberColumnIndex = -1;
+                JSONArray headerRow = valuesArray[0].AsArray;
+                for (int i = 0; i < headerRow.Count; i++)
+                {
+                    if (headerRow[i] == "Number")
+                    {
+                        numberColumnIndex = i;
+                        break;
+                    }
+                }
+
+                if (numberColumnIndex != -1)
+                {
+                    // Collect all mobile numbers and store in the list
+                    for (int i = 1; i < valuesArray.Count; i++)
+                    {
+                        string mobileNumber = valuesArray[i][numberColumnIndex];
+                        mobileNumbers.Add(mobileNumber);
+                    }
+
+                    // Now you have all mobile numbers in the 'mobileNumbers' list
+
+                    // You can check if a number exists in the list with a function
+                    bool numberExists = NumberExistsInList("9999999990");
+                    Debug.Log("Number exists in the list: " + numberExists);
+                }
+                else
+                {
+                    Debug.LogError("Number column not found in the header.");
+                }
+            }
+            else
+            {
+                Debug.LogError("No data found in the values array.");
+            }
         }
+    }
+
+    bool NumberExistsInList(string numberToCheck)
+    {
+        return mobileNumbers.Contains(numberToCheck);
     }
 }
